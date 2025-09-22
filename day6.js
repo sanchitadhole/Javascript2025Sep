7f31997fc2f72b5a6ae1ff46e64edda71a13e015
Sanchita Dhole <sanchitadhole27@gmail.com>
2025-09-22 21:54:25 +0530

    added

---
+++ day6.js
@@ -0,0 +1,26 @@
+// switch case
+// one input and multiple outcome ---switch
+// switch case witout break statement
+
+let city = "sangamner"
+switch(city){
+
+    case "pune":
+        console.log("MH");
+    case "ujjain":
+        console.log("MP");
+    case "jaypur":
+        console.log("RJ");
+    case "sangamner":
+        console.log("M9H")
+
+}
+
+var a = 9;
+var b = 10;
+
+if(a>b){
+    console.log("a greater")
+}else{
+    console.log("b is greater")
+}
