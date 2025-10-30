587b863b78aacbcd52e3ca86dff699ae846b03ad
Sanchita Dhole <sanchitadhole27@gmail.com>
2025-10-30 12:13:11 +0530

    added

---
+++ Programs/day1.js
@@ -0,0 +1,11 @@
+// let str = "Sanchita"
+// let rev = ''
+// for(let i=0;i<str.length;i++){
+// rev = str[i] + rev
+// }
+// console.log(rev)
+
+
+var symbol1 = Symbol('symbol');
+console.log(typeof(symbol1));
+
